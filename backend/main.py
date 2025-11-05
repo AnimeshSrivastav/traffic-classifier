768ef9d7507e5ccd19517a999b8c46bcf77d1bb1
Animesh Srivastav <97078697+AnimeshSrivastav@users.noreply.github.com>
2025-11-05 12:58:07 +0530

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+from torchvision import transforms
+from PIL import Image
+import io
+import torch.nn as nn
+class TrafficSignalClassifier(nn.Module):
+    def __init__(self, num_classes=43):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 16 * 16, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(64, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def home():
+    return {"message": "🚦 API is running! Visit /static/index.html for frontend"}
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = TrafficSignalClassifier(num_classes=43)
+model.load_state_dict(torch.load("traffic_classifier.pth", map_location=device))
+model.eval()
+
+
+transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data)).convert("RGB")
+    image = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(image)
+        _, predicted = torch.max(output, 1)
+        predicted_class = predicted.item()
+
+    return JSONResponse(content={"predicted_class": int(predicted_class)})
